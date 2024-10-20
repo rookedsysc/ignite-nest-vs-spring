0a928811fec6cc9a5f512935781899b4dc8ba73a
rookedsysc <rookedsysc36@gmail.com>
2024-10-20 23:45:49 +0900

    feat(AppModule): 공통 모듈 추가, datasourceConfig 추가

--- ignite-nest/src/app.module.ts
+++ ignite-nest/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { datasourceConfig } from './data-source.config';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(datasourceConfig), CommonModule],
   controllers: [],
   providers: [],
 })
