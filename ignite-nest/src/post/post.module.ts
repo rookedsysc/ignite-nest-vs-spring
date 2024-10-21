cde26a79c2d1f11d487731a3166fb6d574b1efa0
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 20:16:46 +0900

    feat: post module upload adapter 추가

--- ignite-nest/src/post/post.module.ts
+++ ignite-nest/src/post/post.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entity/post.entity';
 import { PostCommandAdapter } from './infrastructure/repository/post-command.adapter';
+import { PostUploadService } from './service/post-upload.service';
+import { POST_OUT_PORT } from './constants/post.token';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
-  providers: [PostCommandAdapter],
+  providers: [
+    PostUploadService,
+    {
+      provide: POST_OUT_PORT,
+      useClass: PostCommandAdapter,
+    },
+  ],
 })
 export class PostModule {}
