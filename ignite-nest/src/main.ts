f9b73c3c6a8af048d295a6cf0f0efb20c56ecaed
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 19:37:38 +0900

    feat: main.ts validation 설정 추가

--- ignite-nest/src/main.ts
+++ ignite-nest/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // for validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 쿼리 매개변수를 자동으로 클래스 인스턴스로 변환
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   // Swagger Setup
   const config = new DocumentBuilder()
