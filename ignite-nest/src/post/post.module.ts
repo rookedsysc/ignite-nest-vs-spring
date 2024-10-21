d35940e8ec2df2e7b79ffd17aac45ae9bb92d32c
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 22:21:25 +0900

    feat: pagination service 추가

--- ignite-nest/src/post/post.module.ts
+++ ignite-nest/src/post/post.module.ts
@@ -5,6 +5,7 @@ import { PostCommandAdapter } from './infrastructure/repository/post-command.ada
 import { POST_OUT_PORT, POST_UPLOAD_USECASE } from './constants/post.token';
 import { PostUploadService } from './service/post-upload.service';
 import { PostUploadController } from './infrastructure/web/post-upload.controller';
+import { PaginationService } from '../common/service/pagination.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
@@ -17,6 +18,7 @@ import { PostUploadController } from './infrastructure/web/post-upload.controlle
       provide: POST_OUT_PORT,
       useClass: PostCommandAdapter,
     },
+    PaginationService,
   ],
   controllers: [PostUploadController],
 })
