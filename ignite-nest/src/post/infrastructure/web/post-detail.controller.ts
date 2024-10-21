489b5edc3888a8ebd314a9437b2426e691ea4279
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 00:31:42 +0900

    fix: inject post detail usecase 지정

--- ignite-nest/src/post/infrastructure/web/post-detail.controller.ts
+++ ignite-nest/src/post/infrastructure/web/post-detail.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
-import { POST_CONTROLLER } from '../../constants/post.token';
+import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  POST_CONTROLLER,
+  POST_DETAIL_USECASE,
+} from '../../constants/post.token';
 import { ApiTags } from '@nestjs/swagger';
 import { PostDetailUsecase } from '../../domain/usecase/post-detail.usecase';
 import { GetPostDetailSwagger } from './decorator/post-detail-controller.decorator';
@@ -8,7 +11,10 @@ import { PostDetailDto } from './dto/response/post-detail.dto';
 @ApiTags(POST_CONTROLLER)
 @Controller(POST_CONTROLLER)
 export class PostDetailController {
-  constructor(private readonly postDetailUsecase: PostDetailUsecase) {}
+  constructor(
+    @Inject(POST_DETAIL_USECASE)
+    private readonly postDetailUsecase: PostDetailUsecase,
+  ) {}
 
   @Get(':postId')
   @GetPostDetailSwagger()
