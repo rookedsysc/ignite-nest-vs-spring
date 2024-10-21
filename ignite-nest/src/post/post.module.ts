2c87396f3d7e0e8f92d799aebef8e587a6bcdc33
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 20:22:20 +0900

    update(PostModule): post upload service 등록 방식 변경

--- ignite-nest/src/post/post.module.ts
+++ ignite-nest/src/post/post.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entity/post.entity';
 import { PostCommandAdapter } from './infrastructure/repository/post-command.adapter';
+import { POST_OUT_PORT, POST_UPLOAD_USECASE } from './constants/post.token';
 import { PostUploadService } from './service/post-upload.service';
-import { POST_OUT_PORT } from './constants/post.token';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   providers: [
-    PostUploadService,
+    {
+      provide: POST_UPLOAD_USECASE,
+      useClass: PostUploadService,
+    },
     {
       provide: POST_OUT_PORT,
       useClass: PostCommandAdapter,
