7fa62c4642b485e7752d75f67c4a1b2eb741cb47
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 23:03:38 +0900

    feat: post list api 구현

--- ignite-nest/src/post/mapper/post.mapper.ts
+++ ignite-nest/src/post/mapper/post.mapper.ts
@@ -1,6 +1,8 @@
 import { PostUploadDto } from '../infrastructure/web/dto/request/post-upload.dto';
 import { Post } from '../entity/post.entity';
 import { PostDetailDto } from '../infrastructure/web/dto/response/post-detail.dto';
+import { PageResponseDto } from '../../common/dto/page-response.dto';
+import { PostListDto } from '../infrastructure/web/dto/response/post-list.dto';
 
 export class PostMapper {
   static toEntity(postUploadDto: PostUploadDto): Post {
@@ -18,5 +20,25 @@ export class PostMapper {
     postDetailDto.createdAt = post.createdAt;
 
     return postDetailDto;
+  }
+
+  static toListResponse(
+    postPageResponse: PageResponseDto<Post>,
+  ): PageResponseDto<PostListDto> {
+    const postListDto = new PageResponseDto<PostListDto>(
+      postPageResponse.total,
+      postPageResponse.currentPage,
+      postPageResponse.size,
+      postPageResponse.items.map((post) => {
+        const postListDto = new PostListDto();
+        postListDto.id = post.id;
+        postListDto.title = post.title;
+        postListDto.commentCount = post.commentCount;
+        postListDto.createdAt = post.createdAt;
+        return postListDto;
+      }),
+    );
+
+    return postListDto;
   }
 }
--- ignite-nest/src/post/post.module.ts
+++ ignite-nest/src/post/post.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entity/post.entity';
 import { PostCommandAdapter } from './infrastructure/repository/post-command.adapter';
-import { POST_OUT_PORT, POST_UPLOAD_USECASE } from './constants/post.token';
+import {
+  POST_IN_PORT,
+  POST_LIST_USECASE,
+  POST_OUT_PORT,
+  POST_UPLOAD_USECASE,
+} from './constants/post.token';
 import { PostUploadService } from './service/post-upload.service';
 import { PostUploadController } from './infrastructure/web/post-upload.controller';
-import { PaginationService } from '../common/service/pagination.service';
+import { PaginationHelper } from '../common/helper/pagination-helper.service';
+import { PostQueryAdapter } from './infrastructure/repository/post-query.adapter';
+import { PostListController } from './infrastructure/web/decorator/post-list.controller';
+import { PostListService } from './service/post-list.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
@@ -15,11 +23,19 @@ import { PaginationService } from '../common/service/pagination.service';
       useClass: PostUploadService,
     },
     {
+      provide: POST_LIST_USECASE,
+      useClass: PostListService,
+    },
+    {
       provide: POST_OUT_PORT,
       useClass: PostCommandAdapter,
     },
-    PaginationService,
+    {
+      provide: POST_IN_PORT,
+      useClass: PostQueryAdapter,
+    },
+    PaginationHelper,
   ],
-  controllers: [PostUploadController],
+  controllers: [PostUploadController, PostListController],
 })
 export class PostModule {}
