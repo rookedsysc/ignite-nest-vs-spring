a252a1db9c677ecea8e997aa6759bb5392a02a62
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 19:55:01 +0900

    feat: post command adapter provider로 등록

--- ignite-nest/src/post/post.module.ts
+++ ignite-nest/src/post/post.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entity/post.entity';
+import { PostCommandAdapter } from './infrastructure/repository/post-command.adapter';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
+  providers: [PostCommandAdapter],
 })
 export class PostModule {}
