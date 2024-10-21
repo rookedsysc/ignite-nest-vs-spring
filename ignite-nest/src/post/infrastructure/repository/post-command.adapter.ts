6572ee51025c1e868aa57a54b1d4b1c575178760
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 20:15:49 +0900

    fix: post command adpater post repository di 추가

--- ignite-nest/src/post/infrastructure/repository/post-command.adapter.ts
+++ ignite-nest/src/post/infrastructure/repository/post-command.adapter.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PostOutPort } from 'src/post/domain/port/out/post-out.port';
 import { Post } from 'src/post/entity/post.entity';
 import { PostUploadDto } from '../web/dto/request/post-upload.dto';
 import { Repository } from 'typeorm';
 import { PostMapper } from 'src/post/mapper/post.mapper';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PostCommandAdapter implements PostOutPort {
-  constructor(private readonly postRepository: Repository<Post>) {}
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
 
   async save(postUploadDto: PostUploadDto): Promise<Post> {
     const post = PostMapper.toEntity(postUploadDto);
