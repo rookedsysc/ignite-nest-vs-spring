592cb65cdfea0d7039a208a07c51ecc9a2c0c2d4
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 23:37:11 +0900

    feat: post command update 구현

--- ignite-nest/src/post/domain/port/out/post-out.port.ts
+++ ignite-nest/src/post/domain/port/out/post-out.port.ts
@@ -1,6 +1,9 @@
 import { PostUploadDto } from '../../../infrastructure/web/dto/request/post-upload.dto';
 import { Post } from '../../../entity/post.entity';
+import { PostUpdateDto } from '../../../infrastructure/web/dto/request/post-update.dto';
 
 export interface PostOutPort {
   save(postUploadDto: PostUploadDto): Promise<Post>;
+
+  update(post: Post, postUpdateDto: PostUpdateDto): Promise<Post>;
 }
--- ignite-nest/src/post/infrastructure/repository/post-query.adapter.ts
+++ ignite-nest/src/post/infrastructure/repository/post-query.adapter.ts
@@ -1,13 +1,27 @@
 import { PostInPort } from '../../domain/port/in/post-in.port';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Post } from '../../entity/post.entity';
 import { PageRequestDto } from '../../../common/dto/page-request.dto';
 import { PageResponseDto } from '../../../common/dto/page-response.dto';
 import { PaginationHelper } from '../../../common/helper/pagination-helper.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostQueryAdapter implements PostInPort {
-  constructor(private readonly paginationService: PaginationHelper) {}
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+    private readonly paginationService: PaginationHelper,
+  ) {}
+
+  async getPostByIdOrThrow(postId: number): Promise<Post> {
+    return await this.postRepository
+      .findOneOrFail({ where: { id: postId } })
+      .catch(() => {
+        throw new NotFoundException(`Post id ${postId} not found`);
+      });
+  }
 
   async getPostList(request: PageRequestDto): Promise<PageResponseDto<Post>> {
     return this.paginationService.paginate(Post, request);
