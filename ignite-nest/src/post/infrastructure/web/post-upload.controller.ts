86b14233129a3180b4c595d0f251a08d4444be17
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 20:31:27 +0900

    fix: @Body() 추가

--- ignite-nest/src/post/infrastructure/web/post-upload.controller.ts
+++ ignite-nest/src/post/infrastructure/web/post-upload.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import {
   POST_CONTROLLER,
   POST_UPLOAD_USECASE,
 } from '../../constants/post.token';
 import { PostUploadUsecase } from '../../domain/usecase/post-upload.usecase';
 import { PostUploadDto } from './dto/request/post-upload.dto';
+import { PostUploadSwagger } from './decorator/post-upload-controller.decorator';
 
 @Controller(POST_CONTROLLER)
 export class PostUploadController {
@@ -14,7 +15,8 @@ export class PostUploadController {
   ) {}
 
   @Post()
-  async uploadPost(postUploadDto: PostUploadDto) {
+  @PostUploadSwagger()
+  async uploadPost(@Body() postUploadDto: PostUploadDto) {
     await this.postUploadUsecase.uploadPost(postUploadDto);
   }
 }
