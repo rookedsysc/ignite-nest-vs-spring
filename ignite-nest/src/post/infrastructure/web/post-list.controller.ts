f76b27c680e5a002fc8fb9671cfa53500f97a338
rookedsysc <rookedsysc36@gmail.com>
2024-10-21 23:16:47 +0900

    feat: PostListController Swagger 적용

--- ignite-nest/src/post/infrastructure/web/post-list.controller.ts
+++ ignite-nest/src/post/infrastructure/web/post-list.controller.ts
@@ -4,6 +4,7 @@ import { PostListUsecase } from '../../domain/usecase/post-list.usecase';
 import { PageRequestDto } from '../../../common/dto/page-request.dto';
 import { PageResponseDto } from '../../../common/dto/page-response.dto';
 import { PostListDto } from './dto/response/post-list.dto';
+import { PageResponseDecorator } from '../../../common/decorator/page-response.decorator';
 
 @Controller(POST_CONTROLLER)
 export class PostListController {
@@ -13,6 +14,7 @@ export class PostListController {
   ) {}
 
   @Get()
+  @PageResponseDecorator(PostListDto)
   async get(
     @Query() request: PageRequestDto,
   ): Promise<PageResponseDto<PostListDto>> {
