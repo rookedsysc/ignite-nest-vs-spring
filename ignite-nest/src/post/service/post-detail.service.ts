285af87c831bafe09a998e9b763083412a47e6a9
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 00:30:36 +0900

    fix: injectable 추가

--- ignite-nest/src/post/service/post-detail.service.ts
+++ ignite-nest/src/post/service/post-detail.service.ts
@@ -1,8 +1,9 @@
-import { Inject } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { POST_IN_PORT } from '../constants/post.token';
 import { PostInPort } from '../domain/port/in/post-in.port';
 import { PostDetailUsecase } from '../domain/usecase/post-detail.usecase';
 
+@Injectable()
 export class PostDetailService implements PostDetailUsecase {
   constructor(
     @Inject(POST_IN_PORT)
