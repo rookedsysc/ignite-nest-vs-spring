98b06fcfba8304b357cfb71883822bfe5bc11f33
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 16:55:37 +0900

    delete: CommentListUsecase commentId Param 삭제

--- ignite-nest/src/comment/domain/usecase/comment-list.usecase.ts
+++ ignite-nest/src/comment/domain/usecase/comment-list.usecase.ts
@@ -1,5 +1,5 @@
 import { CommentListDto } from '../../infrastructure/web/dto/response/comment-list.dto';
 
 export interface CommentListUsecase {
-  execute(commentId: number): Promise<CommentListDto[]>;
+  execute(): Promise<CommentListDto[]>;
 }
