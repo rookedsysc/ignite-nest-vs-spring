ff58c4e0d9f1d038f9862ba6cf33aba215833eff
rookedsysc <rookedsysc36@gmail.com>
2024-10-23 00:21:17 +0900

    feat: PostInPort, QueryAdapter, Repository byPostIdOrThrow 메서드 구현

---
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/application/adapter/PostQueryAdapter.kt
@@ -0,0 +1,15 @@
+package org.rookedsysc.ignitespring.post.application.adapter
+
+import org.rookedsysc.ignitespring.post.domain.port.`in`.PostInPort
+import org.rookedsysc.ignitespring.post.entity.PostEntity
+import org.rookedsysc.ignitespring.post.infrastructure.repository.PostRepository
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+
+class PostQueryAdapter(
+    private val postRepository: PostRepository
+): PostInPort {
+
+    override fun getPostByIdOrThrow(postId: Long): PostEntity {
+        return postRepository.findById(postId).orElseThrow { throw NotFoundException() }
+    }
+}
