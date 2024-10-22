da5381c17625e0a4bd2c43c948d63676fd2f1c6b
rookedsysc <rookedsysc36@gmail.com>
2024-10-23 00:40:53 +0900

    fix: CommentCreateUsecase, Service에 Param PostCreateDto던 부분 CommentCreateDto로 수정

--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/comment/application/service/CommentCreateService.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/comment/application/service/CommentCreateService.kt
@@ -3,9 +3,9 @@ package org.rookedsysc.ignitespring.comment.application.service
 import org.rookedsysc.ignitespring.comment.domain.port.out.CommentOutPort
 import org.rookedsysc.ignitespring.comment.domain.usecase.CommentCreateUsecase
 import org.rookedsysc.ignitespring.comment.entity.CommentEntity
+import org.rookedsysc.ignitespring.comment.infrastructure.web.dto.CommentCreateDto
 import org.rookedsysc.ignitespring.comment.mapper.CommentMapper
 import org.rookedsysc.ignitespring.post.domain.port.`in`.PostInPort
-import org.rookedsysc.ignitespring.post.infrastructure.web.dto.PostCreateDto
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,7 +14,7 @@ class CommentCreateService(
     private val commentOutPort: CommentOutPort
 ): CommentCreateUsecase {
 
-    override fun createComment(postId:Long, postCreateDto: PostCreateDto) {
+    override fun createComment(postId:Long, postCreateDto: CommentCreateDto) {
         // Post 있는지 검증
         postInPort.getPostByIdOrThrow(postId)
         val comment: CommentEntity = CommentMapper.toEntity(postId, postCreateDto)
--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/comment/domain/usecase/CommentCreateUsecase.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/comment/domain/usecase/CommentCreateUsecase.kt
@@ -1,7 +1,7 @@
 package org.rookedsysc.ignitespring.comment.domain.usecase
 
-import org.rookedsysc.ignitespring.post.infrastructure.web.dto.PostCreateDto
+import org.rookedsysc.ignitespring.comment.infrastructure.web.dto.CommentCreateDto
 
 interface CommentCreateUsecase {
-    fun createComment(postId:Long,postCreateDto: PostCreateDto): Unit
+    fun createComment(postId:Long, postCreateDto: CommentCreateDto): Unit
 }
