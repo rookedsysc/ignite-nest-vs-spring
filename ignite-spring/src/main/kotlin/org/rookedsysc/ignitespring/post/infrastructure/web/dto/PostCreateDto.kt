55cfe6af5d5c713a0d14919380e7866385a820ec
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 22:11:28 +0900

    feat: PostCreateDto 에러 메시지 추가

--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/infrastructure/web/dto/PostCreateDto.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/infrastructure/web/dto/PostCreateDto.kt
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotEmpty
 
 data class PostCreateDto(
-    @field:Max(50)
-    @field:NotEmpty
+    @field:Max(50, message = "제목은 50자를 넘을 수 없음")
+    @field:NotEmpty(message = "제목은 필수")
     val title: String,
-    @field:NotEmpty
+    @field:NotEmpty(message = "내용은 필수")
     val content: String
 )
