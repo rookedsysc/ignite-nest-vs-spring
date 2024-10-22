393188fc8771bd7c1dbfc8965635d411fc7b8ef6
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 12:33:52 +0900

    feat: comment module 추가

--- ignite-nest/src/app.module.ts
+++ ignite-nest/src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { datasourceConfig } from './data-source.config';
 import { CommonModule } from './common/common.module';
 import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(datasourceConfig), CommonModule, PostModule],
+  imports: [TypeOrmModule.forRoot(datasourceConfig), CommonModule, PostModule, CommentModule],
   controllers: [],
   providers: [],
 })
