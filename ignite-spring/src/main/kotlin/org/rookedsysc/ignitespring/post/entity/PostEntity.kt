6d3276d9321da71215aa6bac2a6842ee729671ad
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 20:36:32 +0900

    feat: PostEntity 구현

---
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/entity/PostEntity.kt
@@ -0,0 +1,15 @@
+package org.rookedsysc.ignitespring.post.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import org.rookedsysc.ignitespring.common.entity.BaseEntity
+
+@Entity
+class PostEntity: BaseEntity() {
+    @Column(columnDefinition = "varchar(50)", length = 50, nullable = false)
+    var title: String = ""
+    @Column(columnDefinition = "text", nullable = false)
+    var content: String = ""
+    @Column(name = "comment_count", nullable = false)
+    var commentCount: Int = 0
+}
