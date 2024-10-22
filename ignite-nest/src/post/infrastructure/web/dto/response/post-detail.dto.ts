8601ce53f872679211d417b563f13b9e6ecd0dae
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 17:15:21 +0900

    feat: PostDetailDto comments 추가

--- ignite-nest/src/post/infrastructure/web/dto/response/post-detail.dto.ts
+++ ignite-nest/src/post/infrastructure/web/dto/response/post-detail.dto.ts
@@ -1,9 +1,12 @@
 import { PickType } from '@nestjs/swagger';
 import { Post } from '../../../../entity/post.entity';
+import { CommentListDto } from '../../../../../comment/infrastructure/web/dto/response/comment-list.dto';
 
 export class PostDetailDto extends PickType(Post, [
   'id',
   'title',
   'content',
   'createdAt',
-]) {}
+]) {
+  comments: CommentListDto[];
+}
--- ignite-nest/src/post/mapper/post.mapper.ts
+++ ignite-nest/src/post/mapper/post.mapper.ts
@@ -3,6 +3,7 @@ import { Post } from '../entity/post.entity';
 import { PostDetailDto } from '../infrastructure/web/dto/response/post-detail.dto';
 import { PageResponseDto } from '../../common/dto/page-response.dto';
 import { PostListDto } from '../infrastructure/web/dto/response/post-list.dto';
+import { CommentListDto } from '../../comment/infrastructure/web/dto/response/comment-list.dto';
 
 export class PostMapper {
   static toEntity(postUploadDto: PostUploadDto): Post {
@@ -12,12 +13,13 @@ export class PostMapper {
     return post;
   }
 
-  static toResponse(post: Post): PostDetailDto {
+  static toResponse(post: Post, comments: CommentListDto[]): PostDetailDto {
     const postDetailDto = new PostDetailDto();
     postDetailDto.id = post.id;
     postDetailDto.title = post.title;
     postDetailDto.content = post.content;
     postDetailDto.createdAt = post.createdAt;
+    postDetailDto.comments = comments;
 
     return postDetailDto;
   }
