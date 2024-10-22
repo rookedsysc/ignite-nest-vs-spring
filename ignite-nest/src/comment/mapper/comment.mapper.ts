6b13c7fdbd437c4f57eb0c75a043527e8e8f8bb7
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 16:19:12 +0900

    docs: comment -> comment list dto (toResponse) 메서드 추가

--- ignite-nest/src/comment/mapper/comment.mapper.ts
+++ ignite-nest/src/comment/mapper/comment.mapper.ts
@@ -1,9 +1,25 @@
 import { CommentUploadDto } from '../infrastructure/web/dto/request/comment-upload.dto';
 import { Comment } from '../entity/comment.entity';
 import { Post } from '../../post/entity/post.entity';
+import { PostListDto } from '../../post/infrastructure/web/dto/response/post-list.dto';
+import { CommentListDto } from '../infrastructure/web/dto/response/comment-list.dto';
 
 export class CommentMapper {
   static toEntity(post: Post, comment: CommentUploadDto): Comment {
     return new Comment(post, comment.content, comment.content);
+  }
+
+  static toResponse(
+    comment: Comment,
+    postListDto: PostListDto,
+  ): CommentListDto {
+    const commentListDto = {
+      id: comment.id,
+      title: comment.title,
+      content: comment.content,
+      createdAt: comment.createdAt,
+      postListDto,
+    };
+    return commentListDto;
   }
 }
