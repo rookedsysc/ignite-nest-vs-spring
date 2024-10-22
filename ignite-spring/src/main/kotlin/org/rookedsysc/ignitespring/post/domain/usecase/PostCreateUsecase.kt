4529cb304978cf10432691314095aebc60c2a27f
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 21:54:27 +0900

    refactor: PostCreateUsecase 메서드 execute로 변경 및 반환 타입 Unit으로 변경

--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/domain/usecase/PostCreateUsecase.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/domain/usecase/PostCreateUsecase.kt
@@ -3,5 +3,5 @@ package org.rookedsysc.ignitespring.post.domain.usecase
 import org.rookedsysc.ignitespring.post.infrastructure.web.dto.PostCreateDto
 
 interface PostCreateUsecase {
-    fun createPost(postCreateDto: PostCreateDto): Void
+    fun execute(postCreateDto: PostCreateDto): Unit
 }
