057007ab2119853c030c203867568dc20db2c48e
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 14:06:19 +0900

    feat: comment upload usecase, post module import

--- ignite-nest/src/comment/comment.module.ts
+++ ignite-nest/src/comment/comment.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { COMMENT_OUT_PORT } from './constants/comment.token';
+import {
+  COMMENT_OUT_PORT,
+  COMMENT_UPLOAD_USECASE,
+} from './constants/comment.token';
 import { CommentCommandAdapter } from './infrastructure/repository/comment-command.adapter';
+import { CommentUploadService } from './service/comment-upload.service';
+import { PostModule } from '../post/post.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment]), PostModule],
   providers: [
     {
       provide: COMMENT_OUT_PORT,
       useClass: CommentCommandAdapter,
+    },
+    {
+      provide: COMMENT_UPLOAD_USECASE,
+      useClass: CommentUploadService,
     },
   ],
 })
