2629a599a2d8ef66691d47118c8b3fcf47407d6f
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 16:18:53 +0900

    docs: comment list dto 구현

---
+++ ignite-nest/src/comment/infrastructure/web/dto/response/comment-list.dto.ts
@@ -0,0 +1,23 @@
+import { PickType } from '@nestjs/swagger';
+import { Comment } from '../../../../entity/comment.entity';
+import { PostListDto } from '../../../../../post/infrastructure/web/dto/response/post-list.dto';
+
+export class CommentListDto extends PickType(Comment, [
+  'id',
+  'title',
+  'content',
+  'createdAt',
+]) {
+  postListDto: PostListDto;
+
+  constructor(
+    id: number,
+    title: string,
+    content: string,
+    createdAt: Date,
+    postListDto: PostListDto,
+  ) {
+    super(id, title, content, createdAt);
+    this.postListDto = postListDto;
+  }
+}
