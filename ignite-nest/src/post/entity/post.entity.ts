8b1e6e32b61ba83ac17631a7adb6d219348323bc
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 12:26:48 +0900

    feat: post에 comment entity 추가

--- ignite-nest/src/post/entity/post.entity.ts
+++ ignite-nest/src/post/entity/post.entity.ts
@@ -1,14 +1,20 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 import { Base } from '../../common/entity/base';
+import { Comment } from '../../comment/entity/comment.entity';
 
 @Entity('post')
 export class Post extends Base {
   @Column({ type: 'varchar', length: 50, name: 'title' })
   title: string;
+
   @Column({ type: 'text', name: 'content' })
   content: string;
+
   @Column({ type: 'bigint', name: 'comment_count', default: 0 })
   commentCount: number;
+
+  @OneToMany(() => Comment, (comment) => comment.post)
+  comments: Comment[];
 
   update(title?: string, content?: string) {
     this.title = title ?? this.title;
