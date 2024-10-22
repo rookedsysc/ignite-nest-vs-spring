5dba213887ca5da85c912a0a4398c1a502c16031
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 22:36:51 +0900

    fix: Max -> Size로 Validation 변경 Max로 검증하면 Byte로 검증하기 때문에 글자수가 50자 넘지 않아도 Exception 발생

--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/infrastructure/web/dto/PostCreateDto.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/infrastructure/web/dto/PostCreateDto.kt
@@ -2,9 +2,11 @@ package org.rookedsysc.ignitespring.post.infrastructure.web.dto
 
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
 
 data class PostCreateDto(
-    @field:Max(50, message = "제목은 50자를 넘을 수 없음")
+    //! Max로 검증하면 Byte 사이즈로 검증함
+    @field:Size(max = 50, message = "제목은 50자를 넘을 수 없음")
     @field:NotEmpty(message = "제목은 필수")
     val title: String,
     @field:NotEmpty(message = "내용은 필수")
