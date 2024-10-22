cf3c93262cfbabb723a284afbd956ef349d4d37e
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 14:10:34 +0900

    feat: comment.token comment api 그룹 추기

--- ignite-nest/src/comment/constants/comment.token.ts
+++ ignite-nest/src/comment/constants/comment.token.ts
@@ -4,6 +4,7 @@ export const COMMENT_OUT_PORT = 'COMMENT_OUT_PORT' as const;
 
 // Controller
 export const COMMENT_CONTROLLER = '/api/comment' as const;
+export const COMMENT_CONTROLLER_GROUP = '댓글 API' as const;
 
 // usecase interface
 export const COMMENT_UPLOAD_USECASE = 'COMMENT_UPLOAD_USECASE';
