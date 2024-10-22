46bb9610e5f49d5ddad5837625d72191d11b26c2
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 21:59:53 +0900

    delete: PostRepository implement PostOutPort 삭제

--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/infrastructure/repository/PostRepository.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/infrastructure/repository/PostRepository.kt
@@ -1,7 +1,6 @@
 package org.rookedsysc.ignitespring.post.infrastructure.repository
 
-import org.rookedsysc.ignitespring.post.domain.port.out.PostOutPort
 import org.rookedsysc.ignitespring.post.entity.PostEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository: PostOutPort, JpaRepository<PostEntity, Long> {}
+interface PostRepository: JpaRepository<PostEntity, Long> {}
