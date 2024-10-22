13f53d4ef13fd53f820a370aaca44a71b6a6190e
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 14:05:22 +0900

    feat: comment upload dto, comment mapper 개발

---
+++ ignite-nest/src/comment/infrastructure/web/dto/request/comment-upload.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MaxLength } from 'class-validator';
+
+export class CommentUploadDto {
+  @ApiProperty({
+    example: '제목',
+    description: '제목',
+    required: true,
+  })
+  @MaxLength(50)
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: '내용',
+    description: '내용',
+  })
+  @IsString()
+  content: string;
+}
