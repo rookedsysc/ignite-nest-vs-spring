1fdf2399785c50068c1b4ff31a25de620f87ac6d
rookedsysc <rookedsysc36@gmail.com>
2024-10-23 00:34:48 +0900

    feat: PostQueryAdapter Repository Annotation 추가

--- ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/application/adapter/PostQueryAdapter.kt
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/application/adapter/PostQueryAdapter.kt
@@ -4,7 +4,9 @@ import org.rookedsysc.ignitespring.post.domain.port.`in`.PostInPort
 import org.rookedsysc.ignitespring.post.entity.PostEntity
 import org.rookedsysc.ignitespring.post.infrastructure.repository.PostRepository
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.stereotype.Repository
 
+@Repository
 class PostQueryAdapter(
     private val postRepository: PostRepository
 ): PostInPort {
