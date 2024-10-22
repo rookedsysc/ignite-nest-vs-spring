71f802354acff874703f03594b1d4941afc88b7a
rookedsysc <rookedsysc36@gmail.com>
2024-10-22 21:10:44 +0900

    feat: PostOutPort, Adapter, Repository save 메서드 추가

---
+++ ignite-spring/src/main/kotlin/org/rookedsysc/ignitespring/post/service/adapter/PostCommandAdapter.kt
@@ -0,0 +1,16 @@
+package org.rookedsysc.ignitespring.post.service.adapter
+
+import org.rookedsysc.ignitespring.post.domain.port.out.PostOutPort
+import org.rookedsysc.ignitespring.post.entity.PostEntity
+import org.rookedsysc.ignitespring.post.infrastructure.repository.PostRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+class PostCommandAdapter(
+    private val postRepository: PostRepository,
+): PostOutPort {
+
+    override fun save(post: PostEntity): PostEntity {
+        return postRepository.save(post)
+    }
+}
