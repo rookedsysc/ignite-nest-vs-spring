68d689939d6f235d9e13814d256061a14309dba9
rookedsysc <rookedsysc36@gmail.com>
2024-10-24 19:11:57 +0900

    update: Nest Comment 조회 API 주소 comment -> comments 변경 (#12)

--- ignite-nest/src/comment/constants/comment.token.ts
+++ ignite-nest/src/comment/constants/comment.token.ts
@@ -3,7 +3,7 @@ export const COMMENT_IN_PORT = 'COMMENT_IN_PORT' as const;
 export const COMMENT_OUT_PORT = 'COMMENT_OUT_PORT' as const;
 
 // Controller
-export const COMMENT_CONTROLLER = '/api/comment' as const;
+export const COMMENT_CONTROLLER = '/api/comments' as const;
 export const COMMENT_CONTROLLER_GROUP = '댓글 API' as const;
 
 // usecase interface
